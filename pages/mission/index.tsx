14c52133857689e1565bb2fc13f4a08ad0bfabae
Narbeh Shahnazarian <narbwork@gmail.com>
2022-05-01 17:37:11 -0700

    Adds mission page

---
+++ pages/mission/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Heading } from "@chakra-ui/react";
+import PageLayout from "../../components/layouts";
+
+const MissionPage = (): JSX.Element => {
+  return (
+    <PageLayout>
+      <Heading as="h1" textColor={"white"}>
+      🛠 Page under construction 🛠
+      </Heading>
+    </PageLayout>
+  );
+};
+
+export default MissionPage;
