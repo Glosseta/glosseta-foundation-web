429f61c3c96db97d8da66b3b36eb33e17ba411a7
Narbeh Shahnazarian <narbwork@gmail.com>
2022-05-01 17:33:48 -0700

    Adds redirect card; adds link to board, mission and what is glosseta

---
+++ components/navigation/page-redirect-card.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Heading, Box, Text, VStack, Link, TagLabel } from "@chakra-ui/react";
+
+const PageRedirectCard = ({title, navLink} : {title: string, navLink: string}): JSX.Element => {
+  return (
+    <Box
+      width="100%"
+      background="#2C3539"
+      borderWidth="1px"
+      borderColor="white"
+    >
+      <VStack padding={3}>
+        <Link href={navLink}>{title}</Link>
+      </VStack>
+    </Box>
+  );
+};
+
+export default PageRedirectCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from "next";
 import PageLayout from "../components/layouts";
-import {
-  VStack,
-  chakra,
-  Heading,
-  Container,
-} from "@chakra-ui/react";
+import { VStack, chakra, Heading, Container, Image } from "@chakra-ui/react";
+import PageRedirectCard from "../components/navigation/page-redirect-card";
 
 const Home: NextPage = () => {
   return (
@@ -13,9 +9,28 @@ const Home: NextPage = () => {
       <PageLayout>
         <Container maxW={{ base: "sm", sm: "xl" }}>
           <chakra.main textColor={"white"}>
-            <VStack>
-              <Heading as="h1">Glosseta Foundation</Heading>
-            </VStack>
+            <Container marginBottom={"65px"}>
+              <VStack>
+                <Heading as="h1">Glosseta Foundation</Heading>
+              </VStack>
+              <VStack>
+                <Image src="/glosseta_icon.png" alt="Glosseta G Icon" />
+              </VStack>
+            </Container>
+            <Container>
+              <VStack padding={3}>
+                <PageRedirectCard title="Our Mission" navLink="/mission" />
+              </VStack>
+              <VStack padding={3}>
+                <PageRedirectCard title="Meet the team" navLink="/board" />
+              </VStack>
+              <VStack padding={3}>
+                <PageRedirectCard
+                  title="What is Glosseta?"
+                  navLink="/glosseta"
+                />
+              </VStack>
+            </Container>
           </chakra.main>
         </Container>
       </PageLayout>
