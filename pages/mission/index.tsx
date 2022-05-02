287935486a76f207403500a3bc91813b3379c3cd
Narbeh Shahnazarian <narbwork@gmail.com>
2022-05-01 18:35:06 -0700

    Adds content card for page content

--- pages/mission/index.tsx
+++ pages/mission/index.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Heading } from "@chakra-ui/react";
 import PageLayout from "../../components/layouts";
+import ContentCard from "../../components/content/content-card";
 
 const MissionPage = (): JSX.Element => {
   return (
     <PageLayout>
-      <Heading as="h1" textColor={"white"}>
-      🛠 Page under construction 🛠
-      </Heading>
+      <ContentCard title="Mission" content="🛠 Page under construction 🛠"/>
     </PageLayout>
   );
 };
