1b0ac01665d31fffd5b24f0ddb832749be08adea
Narbeh Shahnazarian <narbwork@gmail.com>
2022-05-01 17:38:38 -0700

    Adds what is glosseta page

---
+++ pages/glosseta/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Heading } from "@chakra-ui/react";
+import PageLayout from "../../components/layouts";
+
+const GlossetaPage = (): JSX.Element => {
+  return (
+    <PageLayout>
+      <Heading as="h1" textColor={"white"}>
+      🛠 Page under construction 🛠
+      </Heading>
+    </PageLayout>
+  );
+};
+
+export default GlossetaPage;
