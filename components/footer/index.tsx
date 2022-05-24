f53e6962b8c6cd36115068831bf269ff47dac4f9
Narbeh Shahnazarian <narbwork@gmail.com>
2022-05-23 21:19:26 -0700

    Adds glosseta twitter link to footer; text tweaks

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,16 +1,36 @@
 import React from "react";
-import { chakra, VStack, Text } from "@chakra-ui/react";
+import {
+  chakra,
+  VStack,
+  Text,
+  Link,
+  VisuallyHidden,
+  Icon,
+} from "@chakra-ui/react";
 import { useTranslation } from "next-i18next";
+import { FaTwitter } from "react-icons/fa";
 
 const Footer = (): JSX.Element => {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
 
   return (
     <>
       <chakra.footer isTruncated={false} py={10} color="white">
-          <VStack>
-              <Text>{t("copyright")}</Text>
-          </VStack>
+        <VStack>
+          <Text>{t("copyright")}</Text>
+        </VStack>
+        <VStack>
+          <Link
+            padding={2}
+            href={"https://twitter.com/glossetadotcom"}
+            display="flex"
+            title="twitter"
+            isExternal
+          >
+            <Icon as={FaTwitter} />
+            <VisuallyHidden>{t("TwitterA11yText")}</VisuallyHidden>
+          </Link>
+        </VStack>
       </chakra.footer>
     </>
   );
